b9d97db9c014576f5c03ae6e090b138dad93c995
friformato9 <franco.riformato@nttdata.com>
2025-09-16 14:59:01 +0200

    fixup! feat(fe): add basic ngx-translate loader

--- demos-app/src/app/i18n/dynamic-import-loader.ts
+++ demos-app/src/app/i18n/dynamic-import-loader.ts
@@ -2,7 +2,6 @@ import { TranslateLoader } from '@ngx-translate/core';
 import { from } from 'rxjs';
 
 export class DynamicImportLoader implements TranslateLoader {
-   
   getTranslation(lang: string) {
     const fetchIt = async () => (await import('./it.json')).default;
     const fetchEn = async () => (await import('./en.json')).default;
