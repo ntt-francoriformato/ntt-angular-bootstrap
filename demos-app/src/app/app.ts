1df589b34d4712beec428777cc34658b22c1ffac
friformato9 <franco.riformato@nttdata.com>
2025-09-16 15:01:56 +0200

    feat(fe): use example translation

--- demos-app/src/app/app.ts
+++ demos-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { Button } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
-  imports: [Button, RouterOutlet],
+  imports: [Button, RouterOutlet, TranslateModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
