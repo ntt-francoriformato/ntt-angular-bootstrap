dd3e914d348230662934a9b3656f5b4d83fc161f
friformato9 <franco.riformato@nttdata.com>
2025-09-16 14:42:44 +0200

    feat(fe): provide `primeng`

--- demos-app/src/app/app.config.ts
+++ demos-app/src/app/app.config.ts
@@ -6,11 +6,16 @@ import {
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import { theme } from '../theme/primeng-config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes),
+    providePrimeNG({
+      theme,
+    }),
   ],
 };
