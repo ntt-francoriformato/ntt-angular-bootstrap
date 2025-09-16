666ba6fb56fca1479a85420bf54cbbd48cc89cc8
friformato9 <franco.riformato@nttdata.com>
2025-09-16 16:00:25 +0200

    feat(fe): provide api

--- demos-app/src/app/app.config.ts
+++ demos-app/src/app/app.config.ts
@@ -12,6 +12,9 @@ import { theme } from '../theme/primeng-config';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { DynamicImportLoader } from './i18n/dynamic-import-loader';
 import { environment } from '../environments/environment';
+import { ApiModule as DummyJsonApiModule } from '../connectors/dummy-json-api/api.module';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -22,7 +25,12 @@ export const appConfig: ApplicationConfig = {
       theme,
     }),
     provideAnimations(),
+    provideHttpClient(),
     importProvidersFrom([
+      DummyJsonApiModule.forRoot({
+        rootUrl: environment.apiBaseUrl,
+      }),
+
       TranslateModule.forRoot({
         fallbackLang: localStorage.getItem('lang') ?? environment.defaultLang,
         loader: {
