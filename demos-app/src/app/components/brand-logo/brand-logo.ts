bfcc80b848ff466f8ef3ef247f90275cd1669fad
friformato9 <franco.riformato@nttdata.com>
2025-09-16 15:58:43 +0200

    feat(fe): scaffold simple brand logo in jap
    because its really cool

---
+++ demos-app/src/app/components/brand-logo/brand-logo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-logo',
+  imports: [],
+  templateUrl: './brand-logo.html',
+  styleUrl: './brand-logo.css',
+})
+export class BrandLogo {}
