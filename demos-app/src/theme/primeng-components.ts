c5ed7a85bb5d260604e5dd5680e21de78b36dab1
friformato9 <franco.riformato@nttdata.com>
2025-09-17 12:08:23 +0200

    feat(fe): table style overrides example

--- demos-app/src/theme/primeng-components.ts
+++ demos-app/src/theme/primeng-components.ts
@@ -3,7 +3,14 @@ type ComponentStyle = () => string[];
 /* This is an example of how we are going to override primeng style behaviour */
 const buttons: ComponentStyle = () => [`flex-shrink: 0;`];
 
+const datatable: ComponentStyle = () => [
+  `tr:hover {
+    background-color: var(--color-surface-variant) !important;
+  }`,
+];
+
 // NOTE: always use the same name PrimeNG uses, since this object will be directly used in primeng theme
 export const components = {
-    buttons: { css: () => `.p-button {${buttons().join('')}}` },
+  buttons: { css: () => `.p-button {${buttons().join('')}}` },
+  datatable: { css: () => `.p-datatable {${datatable().join('')}}` },
 };
